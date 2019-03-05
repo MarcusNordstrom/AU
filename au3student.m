f7302c3410d8a8120d464f7a6fbdaef5cf86f0b6
mackan240 <mackan240@gmail.com>
2019-03-05 14:07:34 +0100

    added solar elevationangle

--- au3student.m
+++ au3student.m
@@ -1,4 +1,23 @@
-calcDayOfYear(4, 3)
+% day = calcDayOfYear(5, 3);
+% goteborgLat = 57.70723;
+% declinationAngle = calcDeclinationAngle(day);
+% hourAngle = calcHourAngle(10);
+%calcElevationAngle(goteborgLat, declinationAngle, hourAngle)
+
+
+%%
+%Solar functions
+function elevationAngle = calcElevationAngle(lattitude, declinationAngle, hourAngle)
+    elevationAngle = asind((sind(lattitude)*sind(declinationAngle))+(cosd(lattitude)*cosd(declinationAngle)*cosd(hourAngle)));
+end
+
+function hourAngle = calcHourAngle(time)
+    hourAngle = (15*time)-180;
+end
+
+function declinationAngle = calcDeclinationAngle(dayOfYear)
+    declinationAngle = -23.44*cosd(((360/365)*dayOfYear));
+end
 
 function dayOfYear = calcDayOfYear(day, month)
    dayOfYear = 0;
@@ -7,4 +26,13 @@ function dayOfYear = calcDayOfYear(day, month)
       dayOfYear = dayOfYear + daysInMonths(i);
    end
    dayOfYear = dayOfYear + day;
+end
+%%
+%Common functions
+function y = deg2rad(x)
+    y = x.*pi./180;
+end
+
+function y = rad2deg(x)
+    y = (x.*180)./pi;
 end
