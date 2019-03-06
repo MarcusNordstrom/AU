201cbd9cdae30c4b6629924ebd8eb14f0aeb255a
mackan240 <mackan240@gmail.com>
2019-03-06 23:29:50 +0100

    almost completed assignment, only sunHours and pretty plots left!

--- au3student.m
+++ au3student.m
@@ -1,14 +1,54 @@
-% goteborgLat = 57.70723;
-
+goteborgLat = 57.70723;
+azimut = 150;
+elevation = 25;
+efficiency = 0.15;
+area = 30;
 %write current task here
-%Optimize panelElevationAngle
+%Make plotDay pretty
+%Add in sunHours to all formulas
 
+figure(1);
+plotDay(5, 1, goteborgLat, elevation, azimut, efficiency, area)
+figure(2);
+plotYear(goteborgLat, elevation, azimut, efficiency, area)
+optimalElevationAngle = optimizeElevation(goteborgLat, azimut, efficiency, area)
 %%
+
+%optimize functions
+function optimized = optimizeElevation(lattitude, panelAzimutAngle, efficiency, area)
+    fun = @(x)-1*(calcYearlyPower(lattitude, x, panelAzimutAngle, efficiency, area));
+    optimized = fminbnd(fun, 0, 90);
+end
+
 %plot functions
+function plotYear(lattitude, panelElevationAngle, panelAzimutAngle, efficiency, area)
+    power = zeros(1, 365);
+    days = 1:1:365;
+    for dayOfYear = 1:365
+        declinationAngle = calcDeclinationAngle(dayOfYear);
+        t = 1:0.25:23;
+        for i = 1:length(t)
+            currentTime = t(i);
+            hourAngle = calcHourAngle(currentTime);
+            elevationAngle = calcElevationAngle(lattitude, hourAngle, declinationAngle);
+            azimutAngle = calcAzimutAngle(lattitude, elevationAngle, hourAngle, declinationAngle);
+            surfacePower = calcSurfacePower(elevationAngle);
+            panelPower = calcPanelPower(surfacePower, elevationAngle, azimutAngle, panelElevationAngle, panelAzimutAngle);
+            currentPower = calcTotalPower(efficiency, panelPower, area);
+            power(dayOfYear) = power(dayOfYear) + currentPower;
+        end
+    end
+    scatter(days, (abs(power)/1000));
+    axis([1 365 20 110]);
+    title("År");
+    ylabel("P [KW]");
+    xlabel("tid [dagar]");
+end
+
 function plotDay(month, day, lattitude, panelElevationAngle, panelAzimutAngle, efficiency, area)
     dayOfYear = calcDayOfYear(month, day);
     declinationAngle = calcDeclinationAngle(dayOfYear);
-    t = 5:0.25:19; %time array from 5:00 to 19:00 with 15min intervals
+    t = 1:0.25:23; %time array from 1:00 to 23:00 with 15min intervals
     power = zeros(1, length(t));
     for i=1:length(t)
         currentTime = t(i);
@@ -19,11 +59,30 @@ function plotDay(month, day, lattitude, panelElevationAngle, panelAzimutAngle, e
         panelPower = calcPanelPower(surfacePower, elevationAngle, azimutAngle, panelElevationAngle, panelAzimutAngle);
         power(i) = calcTotalPower(efficiency, panelPower, area);
     end
-    scatter(t, power);
+    scatter(t, abs(power));
+    title([num2str(day),'/',num2str(month)])
     ylabel("P [W]");
     xlabel("tid [h]");
 end
 %calculate functions
+function power = calcYearlyPower(lattitude, panelElevationAngle, panelAzimutAngle, efficiency, area)
+    power = 0;
+    for dayOfYear = 1:365
+        declinationAngle = calcDeclinationAngle(dayOfYear);
+        t = 1:0.25:23;
+        for i = 1:length(t)
+            currentTime = t(i);
+            hourAngle = calcHourAngle(currentTime);
+            elevationAngle = calcElevationAngle(lattitude, hourAngle, declinationAngle);
+            azimutAngle = calcAzimutAngle(lattitude, elevationAngle, hourAngle, declinationAngle);
+            surfacePower = calcSurfacePower(elevationAngle);
+            panelPower = calcPanelPower(surfacePower, elevationAngle, azimutAngle, panelElevationAngle, panelAzimutAngle);
+            currentPower = calcTotalPower(efficiency, panelPower, area);
+            power = power + real(currentPower);
+        end
+    end
+end
+
 function totalPower = calcTotalPower(efficiency, panelPower, area)
     totalPower = efficiency*panelPower*area;
 end
@@ -39,12 +98,10 @@ function panelPower = calcPanelPower(surfacePower, elevationAngle, azimutAngle, 
 end
 
 function surfacePower = calcSurfacePower(elevationAngle)
-    if elevationAngle ~= 0
-        surfacePower = 1.1*1360*(0.7.^((1/sind(elevationAngle)).^0.678));
-    elseif elevationAngle == 90
-        
+    if elevationAngle <= 0
+        surfacePower = 0;   
     else
-        surfacePower = 0;
+        surfacePower = 1.1*1360*(0.7.^((1/sind(elevationAngle)).^0.678));
     end
 end
 
@@ -55,7 +112,7 @@ function azimutAngle = calcAzimutAngle(lattitude, elevationAngle, hourAngle, dec
     else
         %ekv 5
        azimutAngle = 180+acosd((((sind(lattitude)*sind(elevationAngle)))-sind(declinationAngle))/(cosd(lattitude)*cosd(elevationAngle)));
-     end
+    end
 end
 
 function elevationAngle = calcElevationAngle(lattitude, hourAngle, declinationAngle)
